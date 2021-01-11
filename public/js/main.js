86bc993105489621c6d77d5d65d1947e51b269ba
Louise <louisepeiffer@hotmail.com>
2021-01-11 17:03:17 +0100

    exo 1 fini

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+// Exo 1
+// let valeur = prompt('Rentrez qq chose')
+// while (valeur.length == 0) {
+//     valeur = prompt('vassyyyy')
+// }
+
+// let samere = parseInt(prompt('donne un chiffre enculé'))
+// while (samere < 10) {
+//     samere = parseInt(prompt('encore'))
+// }
+
+// Exo 2
+
